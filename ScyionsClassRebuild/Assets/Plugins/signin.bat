cffd673b451f35d47d0ea0b0be680bd86bcfcec7
Scyion (Ken) <kenestep@comcast.net>
2021-08-20 07:05:00 -0700

    Increased size of plane to 10x10

--- ScyionsClassRebuild/Assets/Plugins/signin.bat
+++ ScyionsClassRebuild/Assets/Plugins/signin.bat
@@ -1,3 +1,3 @@
 echo off
 title Sign In to AltspaceVR
-curl -v -d "user[email]=kenestep@comcast.net&user[password]=KgEAltSpace5566!" https://account.altvr.com/users/sign_in.json -c cookie
+curl -v -d "user[email]=kenestep@comcast.net&user[password]=KgEASWB5876!" https://account.altvr.com/users/sign_in.json -c cookie
